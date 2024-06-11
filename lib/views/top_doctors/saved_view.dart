39baef841d121ebb2a885252c7b5d504c2052953
Priyanka <priyankamakwana8002@gmail.com>
2024-06-11 11:35:47 +0530

    - added empty view when list is empty

--- lib/views/top_doctors/saved_view.dart
+++ lib/views/top_doctors/saved_view.dart
@@ -4,6 +4,7 @@ import 'package:medics/views/home/home_controller.dart';
 
 import '../../../styles/color_constants.dart';
 import '../../../styles/text_style.dart';
+import '../../config/app_assets.dart';
 import '../home/home_widgets.dart';
 
 class SavedView extends StatelessWidget {
@@ -23,36 +24,47 @@ class SavedView extends StatelessWidget {
         centerTitle: true,
       ),
       backgroundColor: whiteColor,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // buildSectionHeader('Saved Articles', onTap: () {}),
-              // const SizedBox(height: 20.0),
-              Obx(() {
-                if (controller.articles.isEmpty) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                return Column(
-                  children: controller.articles
-                      .where((article) => article.isSaved)
-                      .map((article) {
-                    return buildArticleCard(
-                      article,
-                      onTap: () {
-                        print(article.time);
-                        controller.onArticleTap(article, !article.isSaved);
-                        // controller.onArticleTap(article.title);
-                      },
-                    );
-                  }).toList(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Obx(() {
+          if (controller.articles.where((article) => article.isSaved).isEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppAssets.emptyCart, width: 150, height: 150),
+                  SizedBox(height: 20),
+                  const Text(
+                    'No Articles are Saved.',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: textColor,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            );
+
+            // return Center(child: CircularProgressIndicator());
+          }
+          return SingleChildScrollView(
+            child: Column(
+              children: controller.articles
+                  .where((article) => article.isSaved)
+                  .map((article) {
+                return buildArticleCard(
+                  article,
+                  onTap: () {
+                    print(article.time);
+                    controller.onArticleTap(article, !article.isSaved);
+                    // controller.onArticleTap(article.title);
+                  },
                 );
-              }),
-            ],
-          ),
-        ),
+              }).toList(),
+            ),
+          );
+        }),
       ),
     );
   }
